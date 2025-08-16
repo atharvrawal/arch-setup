b8f97b37533ec7569f444e65926470bc4614e15f
Atharv <atharvrawal04@gmail.com>
2025-08-16 20:31:11 +0530

    yes

--- hyprland/hyprland-install.sh
+++ hyprland/hyprland-install.sh
@@ -25,4 +25,5 @@ sudo rm -rf ~/.config/waybar
 mkdir -p ~/.config/waybar
 ln -s ~/arch-setup/hyprland/waybar/config.jsonc ~/.config/waybar/config.jsonc >/dev/null 2>&1
 ln -s ~/arch-setup/hyprland/waybar/style.css ~/.config/waybar/style.css >/dev/null 2>&1
+ln -s ~/arch-setup/hyprland/waybar/toggle-waybar.sh ~/.config/waybar/toggle-waybar.sh >/dev/null 2>&1
 echo "waybar symlink successfully setup"
