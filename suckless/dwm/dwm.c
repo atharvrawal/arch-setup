d1bce764b38ed0107fffda43c1886f017eca316c
Atharv <atharvrawal04@gmail.com>
2026-02-01 15:45:01 +0530

    yes

--- suckless/dwm/dwm.c
+++ suckless/dwm/dwm.c
@@ -276,7 +276,7 @@ static Cur *cursor[CurLast];
 static Clr **scheme;
 static Display *dpy;
 static Drw *drw;
-static Monitor *mons, *selmon;
+static Monitor *mons, *selmon, *statmon;
 static Window root, wmcheckwin;
 
 /* configuration, allows nested code to access above variables */
@@ -791,7 +791,7 @@ drawbar(Monitor *m)
 		return;
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == statmon) { /* status is only drawn on selected monitor */
 		char *text, *s, ch;
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		// tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
@@ -2069,7 +2069,7 @@ updategeom(void)
 			else
 				mons = createmon();
 		}
-		for (i = 0, m = mons; i < nn && m; m = m->next, i++)
+		for (i = 0, m = mons; i < nn && m; m = m->next, i++){
 			if (i >= n
 			|| unique[i].x_org != m->mx || unique[i].y_org != m->my
 			|| unique[i].width != m->mw || unique[i].height != m->mh)
@@ -2082,6 +2082,10 @@ updategeom(void)
 				m->mh = m->wh = unique[i].height;
 				updatebarpos(m);
 			}
+			if(i == statmonval)
+				statmon = m;
+		}
+
 		/* removed monitors if n > nn */
 		for (i = nn; i < n; i++) {
 			for (m = mons; m && m->next; m = m->next);
@@ -2095,6 +2099,8 @@ updategeom(void)
 			}
 			if (m == selmon)
 				selmon = mons;
+			if (m == statmon)
+				statmon = mons;
 			cleanupmon(m);
 		}
 		free(unique);
@@ -2200,7 +2206,7 @@ updatestatus(void)
 		statusw += TEXTW(text) - lrpad + 2;						// Modified
 																// Modified
 	}															// Modified
-	drawbar(selmon);
+	drawbar(statmon);
 }
 
 void
