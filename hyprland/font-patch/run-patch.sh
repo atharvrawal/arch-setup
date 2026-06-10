ea7f821ff7bfbce1c3705d0e71c4cc1f95aeb5d3
Atharv <atharvrawal04@gmail.com>
2026-06-10 12:42:17 +0530

    yes

--- hyprland/font-patch/run-patch.sh
+++ hyprland/font-patch/run-patch.sh
@@ -1,5 +1,7 @@
 fontforge -script ~/arch-setup/hyprland/font-patch/patch.pe /usr/share/fonts/TTF/HackNerdFont-Regular.ttf HackCustom.ttf
+mkdir -p ~/.local/share/fonts
 cp ~/arch-setup/hyprland/font-patch/HackCustom.ttf ~/.local/share/fonts/
 fc-cache -fv >/dev/null 2>&1
 echo "✅ Hack Nerd Font Patched and Installed Successfully"
 echo ""
+
